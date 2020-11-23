93021e7cb633783da16e9b24c448a13aff439f21
Atharv-deatheaters <62825797+Atharv-deatheaters@users.noreply.github.com>
2020-11-23 10:58:50 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,50 @@
+
+Welcome = print("Welcome to our bank pls enter your pin")
+pin = input("Enter Your Pin code here : ")
+if ( pin == '2897'):
+    pin == input("Enter your method here :")
+else:
+    print("security breach get out ")
+if (pin ==" Bank balance "):
+    print("you have 14 crores in your bank account")
+
+else:
+    print("You have 14 crores in your bank account")
+
+method = input("your method again please, write again if u just opted for bank balance same for withdraw:")
+
+if( method == "deposit" ):
+    pin == input("how much money u want to deposit , 1000,2000,3000,4000,5000 : ")
+
+elif(method == '1000'):
+    print("1000 ruppes have been deposited ")
+
+elif(method == '2000'):
+    print("2000 ruppes have been deposited")
+
+elif(pin == '3000'):
+    print("3000 ruppes have been deposited")   
+
+elif(pin == '4000'):
+    print("4000 ruppes have been deposited")
+
+elif(pin == '5000'):
+    print("5000 ruppes have been deposited")
+
+elif(pin == '6000'):
+    print("6000 ruppes have been deposited")
+
+elif(pin == '7000'):
+    print("7000 ruppes have been deposited")
+
+elif(pin == '8000'):
+    print("8000 ruppes have been deposited")
+
+elif(pin == '9000'):
+    print("9000 ruppes have been deposited")
+
+elif(pin == '10000'):
+    print("10000 ruppes have been deposited")
+
+else:
+    print("")
